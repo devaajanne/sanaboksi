0022750d7328c7441ac53bb4f6466de26fd2f9d5
devaajanne <devaajanne@gmail.com>
2026-01-28 10:48:24 +0200

    Initialize Gradle project

---
+++ backend/src/main/java/sanaboksi/SanaboksiApplication.java
@@ -0,0 +1,13 @@
+package sanaboksi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanaboksiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanaboksiApplication.class, args);
+	}
+
+}
