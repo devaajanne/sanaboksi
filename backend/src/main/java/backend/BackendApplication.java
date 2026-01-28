feb25ea7acb5895cc2d89d195e62b2bea141ea01
devaajanne <devaajanne@gmail.com>
2026-01-28 11:34:20 +0200

    Edit backend group and package names

--- backend/src/main/java/backend/BackendApplication.java
+++ backend/src/main/java/backend/BackendApplication.java
@@ -1,13 +1,13 @@
-package sanaboksi;
+package backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SanaboksiApplication {
+public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SanaboksiApplication.class, args);
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
 }
