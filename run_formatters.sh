2ba7d099a0f4d5f8816a5187399d07b0fbfe15d2
devaajanne <devaajanne@gmail.com>
2026-01-30 09:44:54 +0200

    issue/#3-add-a-script-to-run-all-formatters * add checks to see if correct directories exist

--- run_formatters.sh
+++ run_formatters.sh
@@ -3,6 +3,10 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Check if correct directories exists
+if [[ ! -d "backend" ]]; then echo "ERROR: backend directory not found"; exit 1; fi
+if [[ ! -d "frontend" ]]; then echo "ERROR: frontend directory not found"; exit 1; fi
+
 # Run backend formatter
 format_backend() {
     (cd backend && ./gradlew spotlessApply)
