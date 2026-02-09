b1fad70d469cbc0687d1a4701f99b0ddc68f9371
devaajanne <devaajanne@gmail.com>
2026-02-09 11:38:54 +0200

    issue/#19-add-and-initialize-database * removed redundant @NotEmpty validation

--- backend/src/main/java/backend/domain/Word.java
+++ backend/src/main/java/backend/domain/Word.java
@@ -6,7 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,7 +23,6 @@ public class Word {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @NotEmpty(message = "Word must not be empty")
   @Size(min = 5, max = 5, message = "Word must have 5 letters.")
   @Column(name = "word")
   private String word;
