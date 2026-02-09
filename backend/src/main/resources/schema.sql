78470d5cbc4f5cd52af38d67cdb1d580d508d995
devaajanne <devaajanne@gmail.com>
2026-02-09 11:28:15 +0200

    issue/#19-add-and-initialize-database * add UNIQUE constraint to word in schema.sql

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS five_letter_words;
 CREATE TABLE IF NOT EXISTS five_letter_words
 (
     id      BIGSERIAL PRIMARY KEY,
-    word    VARCHAR(5) NOT NULL
+    word    VARCHAR(5) NOT NULL UNIQUE
 );
