1efe45f82dc1934b412b5857cc39577200ddc035
devaajanne <devaajanne@gmail.com>
2026-02-13 15:18:02 +0200

    issue/#30-add-endpoint-and-services-to-generate-a-sanaboksi-game-grid * add unit tests for game and utility service * remove placeholder unit test class

---
+++ backend/src/test/java/backend/unit/UtilityServiceUnitTests.java
@@ -0,0 +1,47 @@
+package backend.unit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import backend.service.UtilityService;
+import org.junit.jupiter.api.Test;
+
+public class UtilityServiceUnitTests {
+
+  private final UtilityService utilityService = new UtilityService();
+
+  @Test
+  public void getRandomIntShouldReturnValueBetweenOneAndCount() {
+    int randomInt;
+    int count = 20;
+
+    for (int i = 0; i < 1000; i++) {
+      randomInt = utilityService.getRandomInt(count);
+      assertTrue(randomInt >= 1);
+      assertTrue(randomInt <= count);
+    }
+  }
+
+  @Test
+  public void getRandomIntShouldReturnErrorIfCountIsNegative() {
+    int count = -1;
+    String expectedExceptionMessage = "count must be non-negative.";
+
+    IllegalArgumentException exception =
+        assertThrows(IllegalArgumentException.class, () -> utilityService.getRandomInt(count));
+
+    assertEquals(expectedExceptionMessage, exception.getMessage());
+  }
+
+  @Test
+  public void getRandomIntShouldReturnErrorIfCountIsZero() {
+    int count = 0;
+    String expectedExceptionMessage = "count must not be zero.";
+
+    IllegalArgumentException exception =
+        assertThrows(IllegalArgumentException.class, () -> utilityService.getRandomInt(count));
+
+    assertEquals(expectedExceptionMessage, exception.getMessage());
+  }
+}
