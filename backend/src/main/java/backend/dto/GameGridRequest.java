a9fdb3b8850d3dc4b20a913c469d9a7fc8fe1f1e
devaajanne <devaajanne@gmail.com>
2026-02-18 10:53:25 +0200

    issue/#33-users-can-send-a-filled-game-grid-for-validation * add new DTO types to contain data needed for game grid validation

---
+++ backend/src/main/java/backend/dto/GameGridRequest.java
@@ -0,0 +1,17 @@
+package backend.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class GameGridRequest {
+
+  @NotNull(message = "Game grid cannot be null.")
+  private String[][] gameGrid;
+}
