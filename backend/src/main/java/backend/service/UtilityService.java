a016d8398bbf4e04e7a7ae7a1b1a0c9719d6a93f
devaajanne <devaajanne@gmail.com>
2026-02-18 11:38:55 +0200

    issue/#33-users-can-send-a-filled-game-grid-for-validation * implement backend logic to validate game grids and send a validation result back to frontend * add a new endpoint to receive game grids from frontend and return a validated result

--- backend/src/main/java/backend/service/UtilityService.java
+++ backend/src/main/java/backend/service/UtilityService.java
@@ -1,5 +1,8 @@
 package backend.service;
 
+import backend.dto.GameGridRequest;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +20,21 @@ public class UtilityService {
 
     // Returns an int between 0 (inclusive) and count (exclusive)
     return ThreadLocalRandom.current().nextInt(count);
+  }
+
+  public List<String> getGameGridWords(GameGridRequest gameGridRequest) {
+    String[][] gameGrid = gameGridRequest.getGameGrid();
+    List<String> gameGridWords = new ArrayList<>();
+    StringBuilder stringBuilder = new StringBuilder();
+
+    for (int rowIndex = 0; rowIndex < gameGrid.length; rowIndex++) {
+      for (int columnIndex = 0; columnIndex < gameGrid[rowIndex].length; columnIndex++) {
+        stringBuilder.append(gameGrid[rowIndex][columnIndex]);
+      }
+      gameGridWords.add(stringBuilder.toString().toLowerCase());
+      stringBuilder.setLength(0);
+    }
+
+    return gameGridWords;
   }
 }
