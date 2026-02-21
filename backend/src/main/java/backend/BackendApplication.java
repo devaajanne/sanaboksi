f117ef4040b1404279026ff4b9ee202d388f4dd5
devaajanne <devaajanne@gmail.com>
2026-02-21 17:22:46 +0200

    chore/add-javadoc * format file

--- backend/src/main/java/backend/BackendApplication.java
+++ backend/src/main/java/backend/BackendApplication.java
@@ -3,14 +3,11 @@ package backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Main class for the backend Spring Boot application.
- * Starts the application context.
- */
+/** Main class for the backend Spring Boot application. Starts the application context. */
 @SpringBootApplication
 public class BackendApplication {
 
-    /**
+  /**
    * Entry point for the Spring Boot application.
    *
    * @param args command-line arguments
