5a6e583d8ffb8258d3c803e1e7fe988dceb00e31
devaajanne <devaajanne@gmail.com>
2026-02-24 15:03:40 +0200

    issue/#56-stylish-ui * fix PR comment

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -2,10 +2,8 @@ import { Center, Title } from "@mantine/core";
 
 export default function Header() {
   return (
-    <>
-      <Center>
-        <Title>Sanaboksi</Title>
-      </Center>
-    </>
+    <Center>
+      <Title>Sanaboksi</Title>
+    </Center>
   );
 }
