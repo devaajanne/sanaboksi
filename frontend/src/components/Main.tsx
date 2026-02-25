dc4840191d6349b36d59140312ca843acf9a335b
devaajanne <devaajanne@gmail.com>
2026-02-25 09:44:12 +0200

    issue/#56-stylish-ui * add color palettes for dark and light mode * add a color palette context and provider

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,11 +1,24 @@
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, createTheme } from "@mantine/core";
+import type { MantineColorsTuple } from "@mantine/core";
 import App from "./App";
+import { darkModePalette, lightModePalette } from "../utility/ColorPalettes";
+import { ColorPaletteProvider } from "./context/ColorPaletteContextProvider";
 import "@mantine/core/styles.css";
 
 export default function Main() {
+  const theme = createTheme({
+    colors: {
+      darkModePalette: darkModePalette as MantineColorsTuple,
+      lightModePalette: lightModePalette as MantineColorsTuple,
+    },
+    primaryColor: "lightModePalette",
+  });
+
   return (
-    <MantineProvider>
-      <App />
+    <MantineProvider theme={theme}>
+      <ColorPaletteProvider>
+        <App />
+      </ColorPaletteProvider>
     </MantineProvider>
   );
 }
