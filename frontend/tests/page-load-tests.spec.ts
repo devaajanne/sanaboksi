52fd9574505195721b811ad6e0fea35137beb10f
devaajanne <devaajanne@gmail.com>
2026-03-01 11:54:54 +0200

    chore/add-e2e-test * add e2e test to test that page loads initial game grid with fixed letters

--- frontend/tests/page-load-tests.spec.ts
+++ frontend/tests/page-load-tests.spec.ts
@@ -7,3 +7,52 @@ test.beforeEach(async ({ page }) => {
 test("Header has title", async ({ page }) => {
   await expect(page).toHaveTitle("Sanaboksi");
 });
+
+test("Page fetches initial fixed letters on page load", async ({ page }) => {
+  await page.waitForTimeout(1_000);
+
+  const letters1 = await page
+    .getByRole("textbox", { name: "Sana 1" })
+    .evaluateAll((inputs) =>
+      inputs.map((input) => (input as HTMLInputElement).value),
+    );
+
+  const letters2 = await page
+    .getByRole("textbox", { name: "Sana 2" })
+    .evaluateAll((inputs) =>
+      inputs.map((input) => (input as HTMLInputElement).value),
+    );
+
+  const letters3 = await page
+    .getByRole("textbox", { name: "Sana 3" })
+    .evaluateAll((inputs) =>
+      inputs.map((input) => (input as HTMLInputElement).value),
+    );
+
+  const letters4 = await page
+    .getByRole("textbox", { name: "Sana 4" })
+    .evaluateAll((inputs) =>
+      inputs.map((input) => (input as HTMLInputElement).value),
+    );
+
+  const letters5 = await page
+    .getByRole("textbox", { name: "Sana 5" })
+    .evaluateAll((inputs) =>
+      inputs.map((input) => (input as HTMLInputElement).value),
+    );
+
+  await expect(letters1.filter((l) => l === "").length).toBe(4);
+  await expect(letters1.filter((l) => l !== "").length).toBe(1);
+
+  await expect(letters2.filter((l) => l === "").length).toBe(4);
+  await expect(letters2.filter((l) => l !== "").length).toBe(1);
+
+  await expect(letters3.filter((l) => l === "").length).toBe(4);
+  await expect(letters3.filter((l) => l !== "").length).toBe(1);
+
+  await expect(letters4.filter((l) => l === "").length).toBe(4);
+  await expect(letters4.filter((l) => l !== "").length).toBe(1);
+
+  await expect(letters5.filter((l) => l === "").length).toBe(4);
+  await expect(letters5.filter((l) => l !== "").length).toBe(1);
+});
