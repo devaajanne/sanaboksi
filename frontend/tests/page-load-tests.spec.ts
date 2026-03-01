4f9166b6935fc2e96a5bfb76befbd946a1d3d208
devaajanne <devaajanne@gmail.com>
2026-03-01 12:21:09 +0200

    chore/add-e2e-test * fix PR comments

--- frontend/tests/page-load-tests.spec.ts
+++ frontend/tests/page-load-tests.spec.ts
@@ -8,7 +8,9 @@ test("Header has title", async ({ page }) => {
   await expect(page).toHaveTitle("Sanaboksi");
 });
 
-test("Page fetches initial fixed letters on page load", async ({ page }) => {
+test("Each word row initially has exactly one fixed letter", async ({
+  page,
+}) => {
   await page.waitForTimeout(1_000);
 
   const letters1 = await page
@@ -41,18 +43,18 @@ test("Page fetches initial fixed letters on page load", async ({ page }) => {
       inputs.map((input) => (input as HTMLInputElement).value),
     );
 
-  await expect(letters1.filter((l) => l === "").length).toBe(4);
-  await expect(letters1.filter((l) => l !== "").length).toBe(1);
+  expect(letters1.filter((l) => l === "").length).toBe(4);
+  expect(letters1.filter((l) => l !== "").length).toBe(1);
 
-  await expect(letters2.filter((l) => l === "").length).toBe(4);
-  await expect(letters2.filter((l) => l !== "").length).toBe(1);
+  expect(letters2.filter((l) => l === "").length).toBe(4);
+  expect(letters2.filter((l) => l !== "").length).toBe(1);
 
-  await expect(letters3.filter((l) => l === "").length).toBe(4);
-  await expect(letters3.filter((l) => l !== "").length).toBe(1);
+  expect(letters3.filter((l) => l === "").length).toBe(4);
+  expect(letters3.filter((l) => l !== "").length).toBe(1);
 
-  await expect(letters4.filter((l) => l === "").length).toBe(4);
-  await expect(letters4.filter((l) => l !== "").length).toBe(1);
+  expect(letters4.filter((l) => l === "").length).toBe(4);
+  expect(letters4.filter((l) => l !== "").length).toBe(1);
 
-  await expect(letters5.filter((l) => l === "").length).toBe(4);
-  await expect(letters5.filter((l) => l !== "").length).toBe(1);
+  expect(letters5.filter((l) => l === "").length).toBe(4);
+  expect(letters5.filter((l) => l !== "").length).toBe(1);
 });
