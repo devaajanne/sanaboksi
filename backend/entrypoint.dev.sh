1004ec044ca6c7d8aee5c74d5a6f2d5b87de79c6
devaajanne <devaajanne@gmail.com>
2026-03-26 21:48:51 +0200

    chore/fix-dockerfiles-and-entrypoint-files * consolidate package installations * streamline entrypoint scripts

--- backend/entrypoint.dev.sh
+++ backend/entrypoint.dev.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
+set -e
 
 if [ ! -f /database/database.db ]; then
   if [ -f /workdir/databaseInit/reseed_database.sh ]; then
     chmod +x /workdir/databaseInit/reseed_database.sh
-    cd /workdir/databaseInit
-    ./reseed_database.sh
-    cd /workdir
+    /workdir/databaseInit/reseed_database.sh
   else
     echo "Error: reseed_database.sh not found in /workdir/databaseInit, cannot initialize database."
     exit 1
   fi
 fi
 
-./gradlew classes --continuous --no-daemon & ./gradlew bootRun --no-daemon --args='--spring.profiles.active=dev'
+./gradlew classes --continuous --no-daemon & ./gradlew bootRun --continuous --no-daemon --args='--spring.profiles.active=dev'
