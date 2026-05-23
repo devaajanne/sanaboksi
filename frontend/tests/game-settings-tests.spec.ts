1f5e99f9343e9da793096872cc6492e33076f0e5
devaajanne <devaajanne@gmail.com>
2026-05-23 17:24:47 +0300

    issue/#55-user-can-select-word-length * added tests for game difficulty selection and settings modal visibility

---
+++ frontend/tests/game-settings-tests.spec.ts
@@ -0,0 +1,79 @@
+import { test, expect, Page } from "@playwright/test";
+
+async function getGridLetters(page: Page) {
+  const gridLetters: string[][] = [];
+
+  for (let i = 1; i < 5; i++) {
+    const letters = await page
+      .getByRole("textbox", { name: `Sana ${i}` })
+      .evaluateAll((inputs) =>
+        inputs.map((input) => (input as HTMLInputElement).value),
+      );
+    gridLetters.push(letters);
+  }
+  return gridLetters;
+}
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+});
+
+test("Changing game difficulty to four letters loads a correct new game grid", async ({
+  page,
+}) => {
+  await page.getByRole("button", { name: "Avaa pelin asetukset" }).click();
+
+  await expect(page.getByRole("heading", { name: "Asetukset" })).toBeVisible();
+
+  await page.getByText("4 kirjainta").click();
+  await page.waitForTimeout(1_000);
+
+  await page.getByRole("button", { name: "Sulje" }).click();
+
+  const gridLetters = await getGridLetters(page);
+
+  for (let i = 0; i < gridLetters.length; i++) {
+    expect(gridLetters[i].filter((l) => l === "").length).toBe(3);
+    expect(gridLetters[i].filter((l) => l !== "").length).toBe(1);
+  }
+});
+
+test("Changing game difficulty to six letters loads a correct new game grid", async ({
+  page,
+}) => {
+  await page.getByRole("button", { name: "Avaa pelin asetukset" }).click();
+
+  await expect(page.getByRole("heading", { name: "Asetukset" })).toBeVisible();
+
+  await page.getByText("6 kirjainta").click();
+  await page.waitForTimeout(1_000);
+
+  await page.getByRole("button", { name: "Sulje" }).click();
+
+  const gridLetters = await getGridLetters(page);
+
+  for (let i = 0; i < gridLetters.length; i++) {
+    expect(gridLetters[i].filter((l) => l === "").length).toBe(5);
+    expect(gridLetters[i].filter((l) => l !== "").length).toBe(1);
+  }
+});
+
+test("Changing game difficulty to seven letters loads a correct new game grid", async ({
+  page,
+}) => {
+  await page.getByRole("button", { name: "Avaa pelin asetukset" }).click();
+
+  await expect(page.getByRole("heading", { name: "Asetukset" })).toBeVisible();
+
+  await page.getByText("7 kirjainta").click();
+  await page.waitForTimeout(1_000);
+
+  await page.getByRole("button", { name: "Sulje" }).click();
+
+  const gridLetters = await getGridLetters(page);
+
+  for (let i = 0; i < gridLetters.length; i++) {
+    expect(gridLetters[i].filter((l) => l === "").length).toBe(6);
+    expect(gridLetters[i].filter((l) => l !== "").length).toBe(1);
+  }
+});
