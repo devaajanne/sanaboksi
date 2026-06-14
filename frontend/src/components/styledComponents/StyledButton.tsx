416e47a1510028d73cb2944d42ec8208b4399e96
devaajanne <devaajanne@gmail.com>
2026-06-14 16:17:32 +0300

    issue/#155-add-reusable-components * add top margin to StyledButton

--- frontend/src/components/styledComponents/StyledButton.tsx
+++ frontend/src/components/styledComponents/StyledButton.tsx
@@ -24,6 +24,7 @@ export default function StyledButton({
   const colorPalette = useColorPalette();
   const { isSmViewport } = useViewportContext();
   const BUTTON_BORDER_WIDTH = 2;
+  const BUTTON_MARGIN_TOP = "1rem";
   const BUTTON_TEXT_FONT_SIZE_SMALL = "1rem";
   const BUTTON_TEXT_FONT_SIZE_LARGE = "2rem";
   const buttonTextFontSize =
@@ -51,6 +52,7 @@ export default function StyledButton({
               colorPalette[colorPaletteConstants.PRIMARY_COLOR_0],
             borderColor: colorPalette[colorPaletteConstants.SECONDARY_COLOR_1],
             borderWidth: BUTTON_BORDER_WIDTH,
+            marginTop: BUTTON_MARGIN_TOP,
           },
         }}
       >
