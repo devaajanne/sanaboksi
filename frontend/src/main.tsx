52fb10fbec5d39e2f3d92fb5ff1d2040d3e81e5d
devaajanne <devaajanne@gmail.com>
2026-06-19 21:56:22 +0300

    issue/#171-refactor-file-structure * uådate imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,11 @@ import { StrictMode } from "react";
 import "@fontsource/arvo";
 import "./localization/i18n.ts";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import { MantineProvider } from "@mantine/core";
-import { ViewportContextProvider } from "./context/ViewportContextProvider.tsx";
-import { theme } from "./utils/MantineTheme.tsx";
-import { GameContextProvider } from "./context/GameContextProvider.tsx";
+import { ViewportContextProvider } from "./context/ViewportContextProvider";
+import { theme } from "./utils/MantineTheme";
+import { GameContextProvider } from "./context/GameContextProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
