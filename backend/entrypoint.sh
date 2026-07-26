bd5a7f7f06b40564c47fa6774f03ec75006fb7cc
devaajanne <devaajanne@gmail.com>
2026-07-26 14:56:43 +0300

    issue/#195-configure-sqlite * remove env variable from workflow test * update entrypoint script

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 set -e
 
+# Set active Spring profile
 ACTIVE_SPRING_PROFILE="${SPRING_PROFILES_ACTIVE}"
 echo "Active Spring profile: $ACTIVE_SPRING_PROFILE"
-
-# Export SQLite path to reseed script
-export SQLITE_DB_PATH
-mkdir -p "$(dirname "$SQLITE_DB_PATH")"
 
 # Set SQLite seed file location and command to run the app in development container
 if [ "$ACTIVE_SPRING_PROFILE" = "local-dev" ]; then
@@ -24,6 +21,7 @@ if [ "$ACTIVE_SPRING_PROFILE" = "local-prod" ] || [ "$ACTIVE_SPRING_PROFILE" = "
   RUN_COMMAND="java -jar backend_build.jar"
 fi
 
+# Initialize and seed database
 if [ ! -f "$SQLITE_DB_PATH" ]; then
   echo "Initializing and seeding SQLite database for active Spring profile..."
   /bin/bash "$SQLITE_SEED_FILE"
