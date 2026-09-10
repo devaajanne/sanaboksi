45b5ef0a11a4e456966af6592a45b26bff2987f3
devaajanne <devaajanne@gmail.com>
2026-09-10 18:26:21 +0300

    issue/#224-update-prettier-configs * update Prettier configs

--- frontend/prettier.config.ts
+++ frontend/prettier.config.ts
@@ -2,6 +2,8 @@ import { type Config } from "prettier";
 
 const config: Config = {
   plugins: ["prettier-plugin-organize-imports"],
+  trailingComma: "none",
+  singleAttributePerLine: true
 };
 
 export default config;
